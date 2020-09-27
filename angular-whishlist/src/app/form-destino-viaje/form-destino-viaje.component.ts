c60f319e7808267b9bebe992f409c48161a2724e
unknown <michelly-gomez@hotmail.com>
2020-09-26 19:02:29 -0500

    Validadores personalizados

--- angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 import { destinoViaje } from '../models/destino-viaje.model';
 
 @Component({
@@ -10,13 +10,19 @@ import { destinoViaje } from '../models/destino-viaje.model';
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<destinoViaje>
   fg: FormGroup;
+  minLongitud = 4;
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
     // El "Form Group" nos representa al grupo de elementos del formulario ya construido
     this.fg = fb.group({
       //los "Form Controls" que estamos creando para poder vincular a los "tags" HTML.
-      nombre: ['', Validators.required],
+      nombre: ['', Validators.compose([
+        //"validators.compose", es una primitiva para componer un "array" de validadores
+        Validators.required,
+        this.nombreValidator,
+        this.nombreValidatorParametrizable(this.minLongitud)
+      ])],
       url: ['']
     });
 
@@ -36,4 +42,23 @@ export class FormDestinoViajeComponent implements OnInit {
     return false;
   }
 
+  //tipo de dato de retorno'required': true
+  //Validadores customizados, personalizados
+  nombreValidator(control: FormControl): { [s: string]: boolean } {
+    const l = control.value.toString().trim().length;
+    if (l>0 && l<5){
+      return { invalidNombre: true};
+    }
+    return null;
+  }
+
+  nombreValidatorParametrizable(minLing: number): ValidatorFn {
+    return (control: FormControl) : { [s: string]: boolean } | null => {
+      const l = control.value.toString().trim().length;
+      if (l>0 && l<this.minLongitud){
+        return { minLongNombre: true};
+      }
+      return null;
+    }
+  }
 }
