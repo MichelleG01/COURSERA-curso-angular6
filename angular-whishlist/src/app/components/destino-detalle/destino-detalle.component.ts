242ed8458e118bc68cd7133cafb1308119be76bb
unknown <michelly-gomez@hotmail.com>
2020-10-02 21:14:10 -0500

    Inyección de dependencias al haber una actualizacion v1 a v2

--- angular-whishlist/src/app/components/destino-detalle/destino-detalle.component.ts
+++ angular-whishlist/src/app/components/destino-detalle/destino-detalle.component.ts
@@ -3,18 +3,37 @@ import { ActivatedRoute } from '@angular/router';
 import { destinoViaje } from 'src/app/models/destino-viaje.model';
 import { DestinosApiClient } from 'src/app/models/destinos-api-client.model';
 
+//Si quisieramos sacar una nueva versión 
+class DestinosApiClientViejo {
+  getById(id: String): destinoViaje {
+    console.log('llamando por la clase vieja!');
+    return null;
+  }
+}
+
 @Component({
   selector: 'app-destino-detalle',
   templateUrl: './destino-detalle.component.html',
   styleUrls: ['./destino-detalle.component.css'],
   //Se inyecta directamente la dependencia ya que no son  muchos los componentes que la utiliza
-  providers: [DestinosApiClient]
+  //Pero ahora estamos migrando a la v2 que seria este
+  //providers: [ DestinosApiClient ]
+  providers: [
+    DestinosApiClient,
+    //Para que no nos salga error debemos configurar el proovedor que seria asi:
+    { provide: DestinosApiClientViejo, useExisting: DestinosApiClient }
+    /*Ahora, en lo anterior no hay que inyectarle un DestinosApiClient viejo, sino que hay que inyectarle un 
+      DestinosApiClient. Por un lado, DestinosApiClient y DestinosApiClient viejo tienen que tener 
+      un interfaz, tienen que tener sus funciones públicas (compatibles), en este caso getById, 
+      si no va a tirar error. */
+  ]
 })
 export class DestinoDetalleComponent implements OnInit {
   destino: destinoViaje;
 
-  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClient) {}
-
+  //Y nuestros objetos dependen de esa clase vieja
+  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClientViejo) {}
+  
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.destino = this.destinosApiClient.getById(id);
